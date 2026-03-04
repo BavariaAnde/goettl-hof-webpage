f98fb8a03a93b12790985d396a1fbe2abf28f6bf
Clawi <clawi@users.noreply.github.com>
2026-03-04 23:22:59 +0000

    Point site config to www.goettl-oberbubach.de

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bavariaande.github.io/goettl-hof-webpage',
-	base: '/goettl-hof-webpage'
+	site: 'https://www.goettl-oberbubach.de',
+	base: '/'
 });
